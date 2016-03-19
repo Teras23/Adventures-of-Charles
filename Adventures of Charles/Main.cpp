0fc227c596094812d3c634b6796bf465a921ef8a
Karl Martin Teras <karlnitram@gmail.com>
2016-03-20 00:42:20 +0200

    Updated main and added SDL_net

--- Adventures of Charles/Main.cpp
+++ Adventures of Charles/Main.cpp
@@ -1,7 +1,23 @@
 #include <SDL.h>
+#include <SDL_net.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Test" << std::endl;
+    bool running = true;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDLNet_Init();
+    std::cout << "Initialized" << std::endl;
+    SDL_Event evt;
+    SDL_Window* window = SDL_CreateWindow("AOC", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Surface* screen = SDL_GetWindowSurface(window);
+    while(running) {
+        while(SDL_PollEvent(&evt) != 0) {
+            if(evt.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        SDL_UpdateWindowSurface(window);
+    }
+    SDL_Quit();
     return 0;
 }
